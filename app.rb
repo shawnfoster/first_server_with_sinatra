9be8a9b2d98bbc1f29c925dcd2082a8e74fc1a3f
Shawn Foster <scenefoster@gmail.com>
2016-03-17 12:09:31 -0500

    ruby sinatra yeah

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'bundler'
+Bundler.require
+
+get '/' do
+  phamazon_product = {
+    :name => 'New keyboard',
+    :quantity => 17,
+    :description => 'Generic keyboard',
+    :price => 5.99
+  }.to_json
+end
+
+not_found do
+  {
+    :message => 'The resource you requested could not be found.'
+  }.to_jsone
+end
